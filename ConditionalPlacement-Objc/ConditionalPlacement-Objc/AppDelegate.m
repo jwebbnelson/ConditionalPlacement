016c16a68c04b9ca0456cc57e378be9b94800b55
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-02-01 22:25:21 -0700

    Completed Conditianal Placement
    PreCourse Project 3

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -17,10 +17,29 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
+    int karl;
+    int leonard;
     
+    karl = 10;
+    leonard = 6;
     
+    int karlPlace = karl > 7 ? 1 : 2;
+    int leonardPlace = leonard > 7 ? 1 : 2;
+    
+    if((leonardPlace ==1) && (karlPlace == 1)){
+        NSLog(@"Both teachers are placed 1");
+    }
+    else if (karlPlace == 1){
+        NSLog(@"Karl is placed 1 and Leonard is placed 2");
+    }
+    else if (karlPlace != 1){
+        NSLog(@"Leonard is placed 1 and Karl is placed 2");
+    }
+    else if (leonardPlace != 1){
+        NSLog(@"Karl and Leonard are placed 2");
+    }
     return YES;
+    
 }
 
 @end
